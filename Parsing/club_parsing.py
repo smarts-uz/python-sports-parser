f2e200b982eb5ae8f72fbbc5fb8c972b118146b5
Jamshid Masharipov <masharipovjasmhid569@gmail.com>
2024-10-05 16:17:14 +0500

    actualized

--- Parsing/club_parsing.py
+++ Parsing/club_parsing.py
@@ -1,142 +1,9 @@
-# import sys
-#
-#
-# sys.dont_write_bytecode = True
-#
-# # Django specific settings
-# import os
-# from dotenv import load_dotenv
-# # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm.settings')
-# import django
-#
-# django.setup()
-# load_dotenv()
-# base_path = os.getenv('base_path_club')
-# # Import your models for use in your script
-# from orm.db.models import Club
-# import requests
-# from bs4 import BeautifulSoup
-# from deep_translator import GoogleTranslator
-# from orm.db.models import Competition
-#
-# def fetch_content(url, retries=3, timeout=10):
-#     for attempt in range(retries):
-#         try:
-#             response = requests.get(url, timeout=timeout)
-#             response.raise_for_status()
-#             return response.content
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error fetching {url}: {e}")
-#             if attempt < retries - 1:
-#                 print(f"Retrying ({attempt + 1}/{retries})...")
-#     return None
-#
-#
-# a = Competition.objects.all()
-# for club in a:
-#
-#     url = f'https://www.sports.ru/football/tournament/{club.slug}/table/'
-#
-#     response_content = fetch_content(url)
-#     if response_content is None:
-#         print(f"Failed to fetch {url} after retries.")
-#         continue
-#
-#     country_id = club.country_id
-#     competition_id = club.id
-#
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-#
-#     table = soup.find('div', class_='stat mB6')
-#     table_body = table.find('tbody')
-#     table_row = table_body.find_all('tr')
-#
-#     i = 0
-#     for row in table_row:
-#         name_tag = row.find('a', class_="name")
-#         if not name_tag:
-#             continue
-#
-#         name_ru = name_tag['title']
-#         name_en = GoogleTranslator(source='auto', target='en').translate(name_ru)
-#         club_link = name_tag['href']
-#         slug = club_link.split('/')[-2]
-#
-#         club_response = requests.get(club_link)
-#         club_soup = BeautifulSoup(club_response.content, 'html.parser')
-#
-#         descr_tag = club_soup.find('div', class_="descr")
-#         descr = descr_tag.text if descr_tag else ""
-#
-#         region_tag = club_soup.find('th', string='Страна')
-#         if region_tag:
-#             region_td = region_tag.find_next_sibling('td')
-#             country_ru = region_td.get_text(strip=True) if region_td else ""
-#             country_en = GoogleTranslator(source='auto', target='en').translate(country_ru)
-#         else:
-#             country_en = "N/A"
-#
-#         trener_tag = club_soup.find('th', string='Тренер')
-#         if trener_tag:
-#             trener_td = trener_tag.find_next_sibling('td')
-#             trener_ru = trener_td.get_text(strip=True) if trener_td else ""
-#             trener_en = GoogleTranslator(source='auto', target='en').translate(trener_ru)
-#         else:
-#             trener_en = "N/A"
-#
-#         print(name_en, name_ru, club_link)
-#
-#         club_logo_box = club_soup.find('div', class_="img-box")
-#         club_logo = club_logo_box.find('img')['src']
-#         file_type = club_logo.split('.')[-1]
-#         logo_response = requests.get(club_logo).content
-#         print(f'{name_en}.{file_type}')
-#
-#         club_path = os.path.join(base_path, slug)
-#         os.makedirs(club_path, exist_ok=True)
-#         logo_path = os.path.join(club_path, f'{name_en}.{file_type}')
-#         relative_logo_path = logo_path.split('C:/Users/user/Desktop/Parser/Proliga')[-1].replace('\\', '/')
-#         print(relative_logo_path)
-#
-#         form_img = os.path.join(os.path.dirname(relative_logo_path), 'App.png').replace('\\', '/')
-#         print(form_img)
-#
-#         with open(logo_path, 'wb') as f:
-#             f.write(logo_response)
-#         print(f'{name_en} Saved photo! ')
-#
-#         try:
-#             Club.objects.get(name="lala")
-#             print('Already Exists: ', name_en)
-#         except Club.DoesNotExist:
-#             Club.objects.create(
-#                 name=name_en,
-#                 flag_url=relative_logo_path,
-#                 country_id=country_id,
-#                 name_ru=name_ru,
-#                 club_link=club_link,
-#                 slug=slug,
-#                 native=descr,
-#                 region=country_en,
-#                 trainer=trener_en,
-#                 competition_id=competition_id,
-#                 form_img=form_img
-#             )
-#             i += 1
-#             print(i, 'created: ', name_en)
-
-import os
-import django
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from deep_translator import GoogleTranslator
-
-
 import os
 import django
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
@@ -149,7 +16,6 @@ django.setup()
 
 # Now you can import Django models
 from orm.db.models import Club, Competition
-
 
 # Function to fetch content with retries
 def fetch_content(url, retries=3, timeout=10):
@@ -164,13 +30,12 @@ def fetch_content(url, retries=3, timeout=10):
                 print(f"Retrying ({attempt + 1}/{retries})...")
     return None
 
-
 # Function to update or create a Club instance
 def update_or_create_club(name, **fields):
     clubs = Club.objects.filter(name=name)
     if clubs.count() > 1:
         print(f"Multiple clubs found with name '{name}'. Manual review needed.")
-        return
+        return None, None
 
     if clubs.exists():
         club = clubs.first()
@@ -184,83 +49,94 @@ def update_or_create_club(name, **fields):
 
     return club, created
 
+# Main logic for parsing clubs
+def parse_clubs_for_competition(competition):
+    url = f'https://www.sports.ru/football/tournament/{competition.slug}/table/'
+    response_content = fetch_content(url)
+    if response_content is None:
+        print(f"Failed to fetch {url} after retries.")
+        return
 
-# Main logic
+    soup = BeautifulSoup(response_content, 'html.parser')
+    table = soup.find('div', class_='stat mB6')
+    if not table:
+        print(f"No table found for competition {competition.slug}.")
+        return
+
+    table_body = table.find('tbody')
+    table_rows = table_body.find_all('tr')
+
+    for row in table_rows:
+        name_tag = row.find('a', class_="name")
+        if not name_tag:
+            continue
+
+        name_ru = name_tag['title']
+        name_en = GoogleTranslator(source='auto', target='en').translate(name_ru)
+        club_link = name_tag['href']
+        slug = club_link.split('/')[-2]
+
+        # Fetch and process club details
+        club_response = fetch_content(club_link)
+        if club_response is None:
+            print(f"Failed to fetch {club_link} after retries.")
+            continue
+
+        club_soup = BeautifulSoup(club_response, 'html.parser')
+        descr_tag = club_soup.find('div', class_="descr")
+        descr = descr_tag.text if descr_tag else ""
+
+        region_tag = club_soup.find('th', string='Страна')
+        country_ru = region_tag.find_next_sibling('td').get_text(strip=True) if region_tag else "N/A"
+        country_en = GoogleTranslator(source='auto', target='en').translate(country_ru)
+
+        trener_tag = club_soup.find('th', string='Тренер')
+        trener_ru = trener_tag.find_next_sibling('td').get_text(strip=True) if trener_tag else "N/A"
+        trener_en = GoogleTranslator(source='auto', target='en').translate(trener_ru)
+
+        club_logo_box = club_soup.find('div', class_="img-box")
+        club_logo = club_logo_box.find('img')['src']
+        file_type = club_logo.split('.')[-1]
+        logo_response = fetch_content(club_logo)
+        if logo_response is None:
+            print(f"Failed to fetch logo for {name_en} after retries.")
+            continue
+
+        # Save logo image
+        base_path = os.getenv('base_path_club')
+        club_path = os.path.join(base_path, slug)
+        os.makedirs(club_path, exist_ok=True)
+        logo_path = os.path.join(club_path, f'{name_en}.{file_type}')
+        relative_logo_path = logo_path.replace(os.getenv('base_path_club'), '').replace('\\', '/')
+
+        with open(logo_path, 'wb') as f:
+            f.write(logo_response)
+
+        # Update or create Club instance
+        club, created = update_or_create_club(
+            name=name_en,
+            flag_url=relative_logo_path,
+            country_id=competition.country_id,
+            name_ru=name_ru,
+            club_link=club_link,
+            slug=slug,
+            native=descr,
+            region=country_en,
+            trainer=trener_en,
+            competition_id=competition.id
+        )
+
+        if club:  # Check if club instance was created or updated
+            if created:
+                print(f'{name_en} created successfully. Club ID: {club.id}, Club Link: {club_link}')
+            else:
+                print(f'{name_en} updated successfully. Club ID: {club.id}, Club Link: {club_link}')
+
+# Main function to loop through competitions
 def main():
     competitions = Competition.objects.all()
     for competition in competitions:
-        url = f'https://www.sports.ru/football/tournament/{competition.slug}/table/'
-        response_content = fetch_content(url)
-        if response_content is None:
-            print(f"Failed to fetch {url} after retries.")
-            continue
-
-        soup = BeautifulSoup(response_content, 'html.parser')
-        table = soup.find('div', class_='stat mB6')
-        table_body = table.find('tbody')
-        table_rows = table_body.find_all('tr')
-
-        for row in table_rows:
-            name_tag = row.find('a', class_="name")
-            if not name_tag:
-                continue
-
-            name_ru = name_tag['title']
-            name_en = GoogleTranslator(source='auto', target='en').translate(name_ru)
-            club_link = name_tag['href']
-            slug = club_link.split('/')[-2]
-
-            # Fetch and process club details
-            club_response = requests.get(club_link)
-            club_soup = BeautifulSoup(club_response.content, 'html.parser')
-
-            descr_tag = club_soup.find('div', class_="descr")
-            descr = descr_tag.text if descr_tag else ""
-
-            region_tag = club_soup.find('th', string='Страна')
-            country_ru = region_tag.find_next_sibling('td').get_text(strip=True) if region_tag else "N/A"
-            country_en = GoogleTranslator(source='auto', target='en').translate(country_ru)
-
-            trener_tag = club_soup.find('th', string='Тренер')
-            trener_ru = trener_tag.find_next_sibling('td').get_text(strip=True) if trener_tag else "N/A"
-            trener_en = GoogleTranslator(source='auto', target='en').translate(trener_ru)
-
-            club_logo_box = club_soup.find('div', class_="img-box")
-            club_logo = club_logo_box.find('img')['src']
-            file_type = club_logo.split('.')[-1]
-            logo_response = requests.get(club_logo).content
-
-            # Save logo image
-            base_path = os.getenv('base_path_club')
-            club_path = os.path.join(base_path, slug)
-            os.makedirs(club_path, exist_ok=True)
-            logo_path = os.path.join(club_path, f'{name_en}.{file_type}')
-            relative_logo_path = logo_path.split('C:/Users/user/Desktop/Parser/Proliga')[-1].replace('\\', '/')
-            form_img = os.path.join(os.path.dirname(relative_logo_path), 'App.png').replace('\\', '/')
-
-            with open(logo_path, 'wb') as f:
-                f.write(logo_response)
-
-            # Update or create Club instance
-            club, created = update_or_create_club(
-                name=name_en,
-                flag_url=relative_logo_path,
-                country_id=competition.country_id,
-                name_ru=name_ru,
-                club_link=club_link,
-                slug=slug,
-                native=descr,
-                region=country_en,
-                trainer=trener_en,
-                competition_id=competition.id,
-                form_img=form_img
-            )
-
-            if created:
-                print(f'{name_en} created successfully.')
-            else:
-                print(f'{name_en} updated successfully.')
-
+        parse_clubs_for_competition(competition)
 
 if __name__ == "__main__":
     main()
